5d0e53cfb9a5b5452fb32c59ebbcd29eed83c213
ET <tamagni2002@yahoo.fr>
2024-01-07 10:44:35 +0100

    My First Commit

---
+++ src/main/java/org/example/TestMaino.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class TestMaino {
+    public static void main(String[] args) {
+        System.out.println("Hello Amigo");
+        Person person = new Person("Tata");
+        System.out.println(person.getName());
+    }
+}
